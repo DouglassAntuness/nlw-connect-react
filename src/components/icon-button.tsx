a1c6a203541e29c6c1f715f591094373ecaadc92
Douglas Antunes <douglasantunesbarreto@gmail.com>
2025-02-21 09:34:50 -0300

    Todas as features front-end

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,17 @@
+import type { ComponentProps } from 'react'
+import type React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface IconButtonProps extends ComponentProps<'button'> {}
+
+export function IconButton({ className, ...props }: IconButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300',
+        className
+      )}
+      {...props}
+    />
+  )
+}
